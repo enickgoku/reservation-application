a2269147421c898cff0bf0823763d624104095ef
Nick Engelhardt <enickgoku2@gmail.com>
2022-10-11 12:37:23 -0400

    started final project

---
+++ front-end/src/layout/ReservationsPage/index.js
@@ -0,0 +1,14 @@
+function ReservationsPage() {
+  
+
+  return (
+    <div className="reservations-page">
+      <h1>Reservations</h1>
+      <div className="reservations-container">
+        Hi
+      </div>
+    </div>
+  );
+}
+
+export default ReservationsPage
