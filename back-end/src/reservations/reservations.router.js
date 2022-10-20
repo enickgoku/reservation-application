40ee8d8f6162073e60f0fd6fa0e7b29607e37357
Nick Engelhardt <enickgoku2@gmail.com>
2022-10-20 11:42:43 -0400

    added method not allowed

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,13 +1,10 @@
-/**
- * Defines the router for reservation resources.
- *
- * @type {Router}
- */
+const methodNotAllowed = require("../errors/methodNotAllowed")
 
 const router = require("express").Router()
 const controller = require("./reservations.controller")
 
 router.route("/")
   .get(controller.list)
+  .all(methodNotAllowed)
 
 module.exports = router;
